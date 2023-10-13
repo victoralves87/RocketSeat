3e9ce0569d7d187703a6fa0f4d975268592e008a
victor <voctorralves@gmail.com>
2023-10-12 21:07:16 -0300

    configurando o banco de dados

--- RocketSeat/todolist/src/main/java/br/com/victoralves/todolist/user/UserModel.java
+++ RocketSeat/todolist/src/main/java/br/com/victoralves/todolist/user/UserModel.java
@@ -1,9 +1,56 @@
 package br.com.victoralves.todolist.user;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+
+
+@Entity(name = "tb-users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getname(){
+        return name;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
     
-    public String username;
-    public String name;
-    public String password;
 
 }
